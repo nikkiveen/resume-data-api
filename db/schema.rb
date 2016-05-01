db350fe07da74912cc2ecc1083ae8a32aad04de0
Nikki Veen <nikkiveen@gmail.com>
2016-05-01 11:51:28 -0500

    added education model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501164443) do
+ActiveRecord::Schema.define(version: 20160501165043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "educations", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "degree"
+    t.string   "university_name"
+    t.text     "details"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.date     "start_date"
