ac53985715a236478dc605459369dca55974be21
Dana <danasblack@gmail.com>
2016-05-02 21:01:01 -0500

    added faker data

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501165338) do
-
+ActiveRecord::Schema.define(version: 20160503001630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +35,6 @@ ActiveRecord::Schema.define(version: 20160501165338) do
     t.text     "details"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-
     t.integer  "student_id"
   end
 
