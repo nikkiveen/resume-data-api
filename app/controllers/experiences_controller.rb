6377a7c1dedb959062393430f0b9c39eb15875ce
Nikki Veen <nikkiveen@gmail.com>
2016-05-02 20:49:49 -0500

    defined update method

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -10,5 +10,14 @@ class ExperiencesController < ApplicationController
   end
 
   def update
+    @experience = Experience.find_by(id: params[:id])
+
+    @experience.update(
+      start_date: params[:start_date] || @experience.start_date,
+      end_date: params[:end_date] || @experience.end_date,
+      job_title: params[:job_title] || @experience.job_title,
+      company_name: params[:company_name] || @experience.company_name,
+      details: params[:details] || @experience.details
+    )
   end
 end
