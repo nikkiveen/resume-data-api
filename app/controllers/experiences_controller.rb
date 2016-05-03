49d8b050ccd93c3cbd762ace4956384f0e1f43fc
Nikki Veen <nikkiveen@gmail.com>
2016-05-02 20:44:15 -0500

    defined create method

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,4 +1,11 @@
 class ExperiencesController < ApplicationController
   def create
+    @experience = Experience.create(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details]
+    )
   end
 end
